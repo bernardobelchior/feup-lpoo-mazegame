2cf94c324a4207f94697f2cede62bd47c46e11fe
margaridaviterbo <margaridaviterbo@hotmail.com>
2016-02-23 19:43:35 +0000

    check if is possible to move an entity in a certain direction

--- MazeGame/src/game/Entity.java
+++ MazeGame/src/game/Entity.java
@@ -2,36 +2,32 @@ package game;
 
 public class Entity {
 	int x,y;
-	
+
 	public Entity(int x, int y){
 		this.x = x;
 		this.y = y;
 	}
-	
+
 	public int getX(){
 		return x;
 	}
-	
+
 	public int getY(){
 		return y;
 	}
-	
+
 	public void move(Game.Direction direction){
 		switch(direction){
 		case UP:
-			if(y > 0)
-				y--;
+			y--;
 			break;
 		case DOWN:
-			//if(y < )
 			y++;
 			break;
 		case LEFT:
-			if(x > 0)
-				x--;
+			x--;
 			break;
 		case RIGHT:
-			//if(x < )
 			x++;
 			break;
 		case STAY:
--- MazeGame/src/game/Maze.java
+++ MazeGame/src/game/Maze.java
@@ -4,65 +4,102 @@ public class Maze {
 	private Hero hero;
 	private Dragon dragon;
 	private Sword sword;
-	
+
 	private char maze[][]={{'X','X','X','X','X','X','X','X','X','X'},
-						   {'X',' ',' ',' ',' ',' ',' ',' ',' ','X'},
-						   {'X',' ','X','X',' ','X',' ','X',' ','X'},
-						   {'X',' ','X','X',' ','X',' ','X',' ','X'},
-						   {'X',' ','X','X',' ','X',' ','X',' ','X'},
-						   {'X',' ',' ',' ',' ',' ',' ','X',' ','S'},
-						   {'X',' ','X','X',' ','X',' ','X',' ','X'},
-						   {'X',' ','X','X',' ','X',' ','X',' ','X'},
-						   {'X',' ','X','X',' ',' ',' ',' ',' ','X'},
-						   {'X','X','X','X','X','X','X','X','X','X'}};
-	
+			{'X',' ',' ',' ',' ',' ',' ',' ',' ','X'},
+			{'X',' ','X','X',' ','X',' ','X',' ','X'},
+			{'X',' ','X','X',' ','X',' ','X',' ','X'},
+			{'X',' ','X','X',' ','X',' ','X',' ','X'},
+			{'X',' ',' ',' ',' ',' ',' ','X',' ','S'},
+			{'X',' ','X','X',' ','X',' ','X',' ','X'},
+			{'X',' ','X','X',' ','X',' ','X',' ','X'},
+			{'X',' ','X','X',' ',' ',' ',' ',' ','X'},
+			{'X','X','X','X','X','X','X','X','X','X'}};
+
 	public Maze(){
 		hero = new Hero(1,1);
 		setChar(hero.getX(), hero.getY(), hero.getChar());	
-		
+
 		dragon = new Dragon(1,3);
 		setChar(dragon.getX(), dragon.getY(), dragon.getChar());
-		
+
 		sword = new Sword(1, 8);
 		setChar(sword.getX(), sword.getY(), sword.getChar());
 	}
-	
+
 	public void setChar(int x, int y, char c){
 		maze[y][x] = c;
 	}
-	
+
 	public void unsetChar(int x, int y){
 		maze[y][x] = ' ';
 	}
-	
+
+	public char getChar(int x, int y){
+		return maze[y][x];
+	}
+
 	public void moveHero(Game.Direction direction){
 		unsetChar(hero.getX(), hero.getY());
 		hero.move(direction);
 		setChar(hero.getX(), hero.getY(), hero.getChar());
 	}	
-	
+
+	public boolean canMove(int x, int y, Game.Direction direction){
+		//TODO check for 'S'
+		switch(direction){
+		case UP:
+			if (getChar(x,y-1)=='X'){
+				return false;
+			}
+			break;
+		case DOWN:
+			if (getChar(x,y+1)=='X'){
+				return false;
+			}
+			break;
+		case RIGHT:
+			if (getChar(x+1,y)=='X'){
+				return false;
+			}
+			break;	
+		case LEFT:
+			if (getChar(x-1,y)=='X'){
+				return false;
+			}
+			break;
+		case STAY:
+			return true;
+			//break;	
+		}
+		return true;
+
+
+
+	}
+
 	public void moveDragon(Game.Direction direction){
 		unsetChar(dragon.getX(), dragon.getY());
 		dragon.move(direction);
 		setChar(dragon.getX(), dragon.getY(), dragon.getChar());
 	}
-	
+
 	public void pickUpSword(){
 		hero.equipSword();
 		unsetChar(sword.getX(), sword.getY());
 	}
-	
+
 	public void print(){
-		
+
 		for(int i=0; i<maze.length ;i++){
 			for (int j=0;j<maze[i].length;j++){
 				System.out.print(maze[i][j]);
 			}
 			System.out.println();
 		}
-		
+
 	}
-	
-	
-	
+
+
+
 }

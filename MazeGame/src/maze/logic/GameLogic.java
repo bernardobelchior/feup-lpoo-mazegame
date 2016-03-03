1a028d08ccdbf8e265be568a40271d09496072e5
margaridaviterbo <margaridaviterbo@hotmail.com>
2016-03-03 17:53:02 +0000

    deleted lines of code

--- MazeGame/src/maze/logic/GameLogic.java
+++ MazeGame/src/maze/logic/GameLogic.java
@@ -9,26 +9,26 @@ public class GameLogic {
 	private Maze maze;
 	private CommandLineInterface cli = new CommandLineInterface();
 	private char[][] grid;
-	
+
 	public GameLogic(){
 		grid = new char[][] { { 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X' },
-						  	  { 'X', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'X' }, 
-						  	  { 'X', ' ', 'X', 'X', ' ', 'X', ' ', 'X', ' ', 'X' },
-						  	  { 'X', ' ', 'X', 'X', ' ', 'X', ' ', 'X', ' ', 'X' }, 
-						  	  { 'X', ' ', 'X', 'X', ' ', 'X', ' ', 'X', ' ', 'X' },
-						  	  { 'X', ' ', ' ', ' ', ' ', ' ', ' ', 'X', ' ', ' ' }, 
-						  	  { 'X', ' ', 'X', 'X', ' ', 'X', ' ', 'X', ' ', 'X' },
-						  	  { 'X', ' ', 'X', 'X', ' ', 'X', ' ', 'X', ' ', 'X' }, 
-						  	  { 'X', ' ', 'X', 'X', ' ', ' ', ' ', ' ', ' ', 'X' },
-						  	  { 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X' } };
-		generateRandomMaze();
-		
+			{ 'X', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'X' }, 
+			{ 'X', ' ', 'X', 'X', ' ', 'X', ' ', 'X', ' ', 'X' },
+			{ 'X', ' ', 'X', 'X', ' ', 'X', ' ', 'X', ' ', 'X' }, 
+			{ 'X', ' ', 'X', 'X', ' ', 'X', ' ', 'X', ' ', 'X' },
+			{ 'X', ' ', ' ', ' ', ' ', ' ', ' ', 'X', ' ', ' ' }, 
+			{ 'X', ' ', 'X', 'X', ' ', 'X', ' ', 'X', ' ', 'X' },
+			{ 'X', ' ', 'X', 'X', ' ', 'X', ' ', 'X', ' ', 'X' }, 
+			{ 'X', ' ', 'X', 'X', ' ', ' ', ' ', ' ', ' ', 'X' },
+			{ 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X' } };
+			generateRandomMaze();
+
 			cli.print("What mode would you like to play in?");
 			cli.print("S for Stationary Dragon.");
 			cli.print("R for Random Movement");
 			cli.print("Everything else for Sleeping and Random Movement");
-		
-		maze = new Maze(grid, cli.getGameMode());
+
+			maze = new Maze(grid, cli.getGameMode());
 	}
 
 	public void play(){	
@@ -36,88 +36,80 @@ public class GameLogic {
 			cli.print(maze.toString());
 			nextTurn();
 		}
-		
+
 		cli.print(maze.toString());
-		
+
 		if(maze.getGameState() == GameState.DRAGON_WIN){
 			cli.print("The dragon has won!");
 		} else {
 			cli.print("You have won!");
 		}
 	}
-	
+
 	private void nextTurn(){
 		if(!maze.moveHero(cli.getHeroDirection()))
 			cli.print("You cannot move in this direction");
 		maze.updateDragon();
 	}
-	
+
 	private void generateRandomMaze(){
 		int side; 
 		Random random = new Random();
-		
+
 		cli.print("We are generating a NxN maze.");
 		cli.print("Please introduce your N:");
 		side = cli.getInt();
-		
+
 		while(side < 6){
 			cli.print("Maze too small.");
 			cli.print("Please introduce your N:");
 			side = cli.getInt();
 		}
-		
+
 		grid = new char[side][side];
-		
+
 		//Fill with spaces
 		for(int i = 0; i < grid.length; i++){
 			for(int j = 0; j < grid[i].length; j++){
 				grid[j][i] = ' ';
 			}
 		}
-		
+
 		//Creates horizontal walls
 		for(int i = 0; i < grid.length; i++){
 			grid[i][0] = 'X';
 			grid[i][grid.length-1] = 'X';
 		}
-		
+
 		//Creates vertical walls
 		for(int i = 1; i < grid.length-1; i++){
 			grid[0][i] = 'X';
 			grid[grid.length-1][i] = 'X';
 		}
-		
+
 		int x,y;
-		
+
 		//Generates Hero
 		x = random.nextInt(side-2)+1;
-		cli.print(Integer.toString(x));
 		y = random.nextInt(side-2)+1;
-		cli.print(Integer.toString(y));
 		grid[y][x] = 'H';
-		
+
 		//Generates Dragon
 		x = random.nextInt(side-2)+1;
-		cli.print(Integer.toString(x));
 		y = random.nextInt(side-2)+1;
-		cli.print(Integer.toString(y));
 		grid[y][x] = 'D';
-		
+
 		//Generates Sword
 		x = random.nextInt(side-2)+1;
-		cli.print(Integer.toString(x));
 		y = random.nextInt(side-2)+1;
-		cli.print(Integer.toString(y));
 		grid[y][x] = 'E';
-		
+
 		//Generates Exit
 		do{
 			x = random.nextInt(side);
 			y = random.nextInt(side);	
 		} while(((x == 0 || x == side-1) && (y == 0 || y == side -1)) || 	//Checks if the exit is on the corner
 				((x != 0 && x != side -1) && (y != 0 && y != side-1))); 	//Checks if the exit is in the middle of the maze
-		cli.print(Integer.toString(x));
-		cli.print(Integer.toString(y));
 		grid[y][x] = 'S';
 	}
 }
